da81873d3373a6c358d1e4d2c6c83833955607b9
Carlos Wendell Pereira dos Santos <carloswendell@prodam.sp.gov.br>
2025-12-14 01:59:47 -0300

    first commit

---
+++ real-time-notification/Api/NotificationController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace real_time_notification.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+    }
+}
